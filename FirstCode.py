e13e8a08eb8b00094b0e8ca01c7b4202dd7619ad
배민경 <bmk0703@naver.com>
2025-02-08 16:20:21 +0900

    Fix 2025 -> 2026

--- FirstCode.py
+++ FirstCode.py
@@ -11,13 +11,6 @@ for i in name:
 
 def age():
     birth = int(input("태어난 년도를 입력해 주세요 : "))
-    age = 2025 - birth
+    age = 2026 - birth
     print(f"2025년에 당신의 나이는 { age }입니다.")
-
-
-def user_info(name):
-    name = input("이름을 입력하세요: ")
-    print("이름: ", name)
-    index = name.index(name)
-    print("MBTI: ", mbti[index])
-    print("생년월일: ", birth[index])
+age()
