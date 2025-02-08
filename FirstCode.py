336e6ac991f1ce46d213ee66bf1447cacbff4253
canit0221 <rladydtn78@gmail.com>
2025-02-08 15:27:27 +0900

    add user_info feature

--- FirstCode.py
+++ FirstCode.py
@@ -1,6 +1,17 @@
 name = ["김민서", "이건호", "김용수", "배민경"]
 
 word = "파이팅"
-mbti = ["INTP", "ENTJ"]
+mbti = ["ENFJ", "ENTP", "INTP", "ENTJ"]
+birth = [1998, 1994, 1998, 2000]
+
+
 for i in name:
     print(i + " " + word)
+
+
+def user_info(name):
+    name = input("이름을 입력하세요: ")
+    print("이름: ", name)
+    index = name.index(name)
+    print("MBTI: ", mbti[index])
+    print("생년월일: ", birth[index])
